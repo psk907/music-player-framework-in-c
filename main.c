3c11c2cef49224ed8b1c1bac9ab118674c3025bf
Suraj <p.surajkumar907@gmail.com>
2020-11-28 11:34:48 +0530

    createSong made

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_STRING_SIZE 20
 
@@ -14,17 +15,43 @@ typedef struct _Song {
 } Song ;
 
 
-Song* createSong (const char* title){
-    //,const char* artist,const char* album,const int year,const double duration
+// head points to the last song in the playlist(queue)
+Song* head = NULL;
+
+// tail points to the first song in the playlist(queue)
+Song* tail = NULL; 
+
+//Function to to check if the playlist is empty
+bool isEmpty(){
+    return head==NULL;
+}
+
+// nowPlaying points to the song currently playing
+Song* nowPlaying = NULL;
+
+Song* createSong (const char* title,const char* artist,const char* album,const int year,const double duration){
     Song* temp = malloc(sizeof(Song));
     strcpy(temp->title, title); 
+    strcpy(temp->artist, artist); 
+    strcpy(temp->album, album);
+    temp->year=year;
+    temp->duration=duration; 
     return temp;
 }
 
 int main(){
-    Song *song= createSong("Heyya");
-    printf("%s",song->title);
-    printf("Bye world");
+    char userChoice = '0';
+    /* TODO : Write a menu based interface with options and in a loop 
+        1. Add a new song to the playlist
+            - take user inputs
+            - call createSong
+            -  assign it to a pointer
+        X. End program
+    */
+    printf("MENU");
+    
+    Song *song= createSong("Dynamite","BST","Who knows?",2020,92);
+    printf("%s",song->album);
     free(song);
     return 0;
 }
