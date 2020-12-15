fb68f246972efce1b32721724790dc2aded76cac
trishireddy <69588829+trishireddy@users.noreply.github.com>
2020-12-15 21:23:43 +0530

    Create add_to_play.c

---
+++ add_to_play.c
@@ -0,0 +1,48 @@
+void add_to_pl(){
+    int song_number = 0;
+    printf("\nSelect a song to add from the list below");
+    for (i = 0; i < 10 && song_pool[i] != NULL; i++)
+    {
+        printf("\n song no:%d,name:%s \n", (i + 1), song_pool[i]->title); // pool=array of song structures
+        
+    }
+    printf("\nchoose a song number\n");
+    scanf("%d", song_number);
+    if (song_number > 0 && song_number <= 100)
+    node_ptr temp = (node_ptr)malloc(sizeof(PlaylistNode));
+    {
+        if(header_node == NULL){
+            
+            temp->song = song_pool[song_number - 1];
+            temp->prev_song = NULL;
+            temp->next_song = NULL;
+            header_node = temp;
+            tail_node =temp->next_song; 
+        }
+            
+            else if(header_node->next_song == NULL)
+            {
+                temp->song = song_pool[song_number - 1];
+                header_node->next_song = temp;
+                temp->prev_song =head;
+                temp->next_song = NULL;
+                
+                
+            }
+        
+            
+        
+        else{
+            temp->song = song_pool[song_number - 1];
+            tail_node=temp;
+            temp->prev_song = tail_node;
+            temp->next_song = NULL;
+            
+            
+            
+        }
+    }
+    
+    
+    
+}
