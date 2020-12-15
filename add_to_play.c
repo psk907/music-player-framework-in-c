f2201d4f7af5e54134f8d683fba657b35f7ae442
trishireddy <69588829+trishireddy@users.noreply.github.com>
2020-12-16 01:20:50 +0530

    code for shuffle playlist done gn.

--- add_to_play.c
+++ add_to_play.c
@@ -60,3 +60,44 @@ void delete_playlist()
   printf ("playlist does not exist, can't delete");
 
 }
+shuffle_pl(){
+    split_pl()
+        
+{
+        node_ptr slow, fast,header_node2;
+        
+        if(header_node->next_song==NULL) /*only one element*/
+                return 0;
+ 
+        slow=fast=header_node;
+ 
+        while(fast->next_song!=NULL && fast->next_song->next_song!=NULL)
+        {
+                slow = slow->next_song;
+                fast = fast->next_song->next_song;
+        }
+        header_node2 = slow->next_song;
+        header_node2->prev_song=NULL;
+        slow->link = NULL;
+}
+
+interleave()//partially works for even no of songs
+{
+     node_ptr current_node1,current_node2;
+     for(current_node2=header_node2;header_node2->next_song !=0;header_node2=current_node2)
+     {
+         for(current_node1=header_node;header_node->next_song !=0;header_node=current_node1)
+	 {
+             current_node1=header_node->next_song;
+             header_node->next_song=header_node2->next_song;
+             header_node2->next_song=header_node;
+             header_node->prev_song=header_node2;
+             current_node2=header_node;
+		 // display  the playlist if u want
+          }
+         
+     }
+    
+}
+printf("shuffled successfully");
+    }
